676f2abc8bcce778cf2f146c159e9986b8ad698a
DESKTOP-CF5R1ST\H-cym <cgd214@gmail.com>
2017-11-25 23:30:06 +0800

    base

--- routes/index.py
+++ routes/index.py
@@ -10,3 +10,8 @@ main = Blueprint('index', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/todo')
+def base():
+    return render_template('base.html')
